7ef920299a77a77386f3f055ed46f691b4a454de
facebooktool446 <74920725+facebooktool446@users.noreply.github.com>
2020-11-23 23:14:26 +0700

    Add files via upload

---
+++ matkhau.py
@@ -0,0 +1,162 @@
+import os, sys, re, socket
+from time import sleep
+
+red='\u001b[31;1m'
+green='\u001b[32;1m'
+blue='\u001b[34;1m'
+yellow='\u001b[33;1m'
+reset='\u001b[0m'
+
+os.system('clear')
+
+Username = input(green+"Tài Khoản Tool: ")
+Password = input(green+"Mật Khẩu Tool: ")
+
+if Username =="facebooktool"and Password=="facebooktool":
+	  print(yellow+"Logged in successfully")
+	  print (red+"Vui lòng đợi....")
+	  sleep(4)
+	  os.system('clear')
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		   /$$    /$$$$$$ 
+		 /$$$$   /$$$_  $$
+		|_  $$  | $$$$\ $$
+		  | $$  | $$ $$ $$
+		  | $$  | $$\ $$$$
+		  | $$  | $$ \ $$$
+		 /$$$$$$|  $$$$$$/
+		|______/ \______/               
+ """)
+	  sleep(1)
+	  os.system('clear')
+	  print(red+'Vui lòng đợi....')                   
+	  print(yellow+"""
+		  /$$$$$$ 
+		 /$$__  $$
+		| $$  \ $$
+		|  $$$$$$$
+		 \____  $$
+		 /$$  \ $$
+		|  $$$$$$/
+		 \______/ 			 
+""")  
+
+	  sleep(1)
+	  os.system('clear')
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		  /$$$$$$ 
+		 /$$__  $$
+		| $$  \ $$
+		|  $$$$$$/
+		 >$$__  $$
+		| $$  \ $$
+		|  $$$$$$/
+		 \______/ 
+""")
+	  sleep(1)
+	  os.system('clear')
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		 /$$$$$$$$
+		|_____ $$/
+		     /$$/ 
+		    /$$/  
+		   /$$/   
+		  /$$/    
+		 /$$/     
+		|__/      
+ """)
+	  sleep(1)
+	  os.system('clear')
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		  /$$$$$$ 
+		 /$$__  $$
+		| $$  \__/
+		| $$$$$$$ 
+		| $$__  $$
+		| $$  \ $$
+		|  $$$$$$/
+		 \______/ 
+ """)
+	  sleep(1)
+	  os.system('clear')
+
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		 /$$$$$$$ 
+		| $$____/ 
+		| $$      
+		| $$$$$$$ 
+		|_____  $$
+		 /$$  \ $$
+		|  $$$$$$/
+		 \______/ 
+ """)
+	  sleep(1)
+	  os.system('clear')
+
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		 /$$   /$$
+		| $$  | $$
+		| $$  | $$
+		| $$$$$$$$
+		|_____  $$
+		      | $$
+		      | $$
+		      |__/
+""")
+	  sleep(1)
+	  os.system('clear')
+
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		  /$$$$$$ 
+		 /$$__  $$
+		|__/  \ $$
+		   /$$$$$/
+		  |___  $$
+		 /$$  \ $$
+		|  $$$$$$/
+		 \______/ 
+""")
+	  sleep(1)
+	  os.system('clear')
+
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		  /$$$$$$ 
+		 /$$__  $$
+		|__/  \ $$
+		  /$$$$$$/
+		 /$$____/ 
+		| $$      
+		| $$$$$$$$
+		|________/
+""")
+	  sleep(1)
+	  os.system('clear')
+
+	  print(red+'Vui lòng đợi....')
+	  print(yellow+"""
+		   /$$  
+		 /$$$$  
+		|_  $$  
+		  | $$  
+		  | $$  
+		  | $$  
+		 /$$$$$$
+		|______/
+""")
+	  sleep(1)
+	  os.system('clear')
+	  os.system('python tooltest.py')
+	  
+	  				
+else:
+	print(red+"Login failed")
+	sleep(1)
+	os.system('exit')	
